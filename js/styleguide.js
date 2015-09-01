a8d212842e1e551e0c2c8a1bc2dce0daf689132a
Phil Cable <pcable@wsu.edu>
2015-09-01 08:35:00 -0700

    Table of contents disclosure

--- js/styleguide.js
+++ js/styleguide.js
@@ -1,8 +1,8 @@
 (function($){
 
-	$( '.style-guide-toc li:not(.current_page_ancestor):not(.current_page_item) ul' ).hide();
+	$( '.style-guide-toc.article li:not(.current_page_ancestor):not(.current_page_item) ul' ).hide();
 
-	$( '.style-guide-toc li.current_page_item > a > span, .style-guide-toc li.current_page_ancestor > a > span' ).addClass( 'disclosed' );
+	$( '.style-guide-toc.article li.current_page_item > a > span, .style-guide-toc li.current_page_ancestor > a > span, .style-guide-toc:not(.article) li a span' ).addClass( 'disclosed' );
 
 	$( '.style-guide-toc a' ).on( 'click', 'span', function( event ) {
 		event.preventDefault();
--- templates/table-of-contents.php
+++ templates/table-of-contents.php
@@ -1,4 +1,4 @@
-<ul class="style-guide-toc">
+<ul class="style-guide-toc<?php if ( is_single() ) echo ' article'; ?>">
 <?php
 	wp_list_pages( array(
 		'link_before' => '<span></span>',
